c8d852e57642cc09ce70d91359c024619ed9f45c
2p01 <nexablelikesrocks@gmail.com>
2023-12-11 13:19:58 +0100

    .

--- Javascript/T00 - Tematy/T22/T221/main.js
+++ Javascript/T00 - Tematy/T22/T221/main.js
@@ -2,3 +2,14 @@ function wartoscBezwzgledna1(a){
     if(a>0) return a;
     else return a*(-1);
 }
+
+const nr = document.querySelector('#liczba')
+const wynik = document.querySelector('#wynik')
+const btn = document.querySelector('button')
+addEventListener('click', function (){
+    let liczba = parseInt(nr.value)
+    let wynik = wartoscBezwzgledna1(nr)
+
+    console.log(wartoscBezwzgledna1())
+    document.write(wartoscBezwzgledna1())
+})
